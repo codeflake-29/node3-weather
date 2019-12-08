3a47739d9b1d3f6eaa110fe4ee7c1427b33ad2f2
someone <someone@someplace.com>
2019-12-08 19:18:18 +0530

    setup app for heroku

--- source/app.js
+++ source/app.js
@@ -2,6 +2,9 @@ const path=require('path')
 const express=require('express')
 const hbs=require('hbs')
 const app=express()
+
+
+const port=process.env.PORT || 3000
 const geocode=require('./utils/geocode')
 const forcast=require('./utils/forcast')
 
@@ -127,6 +130,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log('server is p at5 porfet')
+app.listen(port,()=>{
+    console.log('server is at'+port)
 })
